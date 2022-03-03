b2f28d6e09b96719f25ffec436aec5ade0e7f6fc
mvrius69 <xmariusro@gmail.com>
2022-03-03 21:35:40 +0200

    change

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import RecipeIngredientEdit from './RecipeIngredientEdit'
 import { RecipeContext } from './App'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 export default function RecipeEdit({ recipe }) {
     const { handleRecipeChange, handleRecipeSelect } = useContext(RecipeContext) 
